79af78b0a6597b560c7e921d1b4768378ce69d8b
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-03-18 16:44:27 +0100

    update - added results and finalize the train

--- train_covid19.py
+++ train_covid19.py
@@ -34,7 +34,7 @@ args = vars(ap.parse_args())
 
 # let's initialize the initial learning rate, number of epoches to train for and the batch size
 INIT_LR = 1e-3  # initial learning rate
-EPOCHES = 25  # number of epoches
+EPOCHS = 25  # number of epoches
 BS = 8  # Batch size
 
 # grab the list of images in our dataset directory, then initialize
@@ -70,7 +70,7 @@ labels = to_categorical(labels)
 
 # partition the data into training and testing splits using 80% of
 # the data for training and the remaining 20% for testing
-(trainX, testX, trainY, trainY) = train_test_split(
+(trainX, testX, trainY, testY) = train_test_split(
     data, labels, test_size=0.20, stratify=labels, random_state=42)
 
 # initialize the training data augmentation object
@@ -78,7 +78,7 @@ trainAug = ImageDataGenerator(rotation_range=15, fill_mode="nearest")
 
 # load the VGG16 network, ensuring the head FC layer sets are left off
 baseModel = VGG16(weights='imagenet', include_top=False,
-                  input_tensor=Input(shape=(224, 244, 3)))
+                  input_tensor=Input(shape=(224, 224, 3)))
 
 # construct  the head of the model that will be placed on top of the base model
 headModel = baseModel.output
@@ -90,3 +90,66 @@ headModel = Dense(2, activation="softmax")(headModel)
 
 # place the head FC model on top of the base model(it will be the actual model which will be use at this project)
 model = Model(inputs=baseModel.input, outputs=headModel)
+
+# loop over all layers in the base model and freeze them so they will not be updated during the first training process
+for layer in baseModel.layers:
+    layer.trainable = False
+
+# let's compile the model
+print("[INFO] compiling model...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+# train the head of the network
+print("[INFO] training head...")
+H = model.fit_generator(trainAug.flow(trainX, trainY, batch_size=BS),
+                        steps_per_epoch=len(trainX) // BS,
+                        validation_data=(testX, testY),
+                        validation_steps=len(testX) // BS,
+                        epochs=EPOCHS)
+
+# make predictions on the testing set
+print("[INFO] evaluating network...")
+predIdxs = model.predict(testX, batch_size=BS)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+
+# show a nicely formatted classification report
+print(classification_report(testY.argmax(axis=1), predIdxs,
+                            target_names=lb.classes_))
+
+# compute the confusion matrix and and use it to derive the raw
+# accuracy, sensitivity, and specificity
+cm = confusion_matrix(testY.argmax(axis=1), predIdxs)
+total = sum(sum(cm))
+acc = (cm[0, 0] + cm[1, 1]) / total
+sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
+specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
+
+
+# show the confusion matrix, accuracy, sensitivity, and specificity
+print(cm)
+print("acc: {:.4f}".format(acc))
+print("sensitivity: {:.4f}".format(sensitivity))
+print("specificity: {:.4f}".format(specificity))
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy on COVID-19 Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
+
+# serialize the model to disk
+print("[INFO] saving COVID-19 detector model...")
+model.save(args["model"], save_format="h5")
